79f4826fbcf4e1ff109acafa29e4b03ef3f6f9c3
jiags <1355690989@qq.com>
2020-11-16 16:47:29 +0800

    "修改了test功能"

--- 基础语法/src/demo24Stream/Test.java
+++ 基础语法/src/demo24Stream/Test.java
@@ -5,5 +5,17 @@ import java.util.List;
 
 public class Test {
     //第一支队伍
-    List<String> list1=new ArrayList<>();
+    public static void main(String[] args) {
+        List<String> one=new ArrayList<>();
+        one.add("迪丽热巴");
+        one.add("宋远桥");
+        one.add("苏星河");
+        one.add("石破天");
+        one.add("石中玉");
+        one.add("老子");
+        one.add("庄子");
+        one.add("洪七公");
+
+    }
+
 }
